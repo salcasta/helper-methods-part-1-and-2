a978ed254f9c54e30d6550ca139b643f2fcb2c3b
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-20 21:39:34 +0000

    refactor complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,7 +17,7 @@ class MoviesController < ApplicationController
       end
 
       format.html do
-        render({ :template => "movies/index" })
+        render template: "movies/index" 
       end
     end
   end
@@ -39,7 +39,7 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to movies_url, :notice => "Movie created successfully."
+      redirect_to movies_url, notice: "Movie created successfully."
     else
       render template: "movies/new"
     end
@@ -64,9 +64,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to movie_url(the_movie), notice: "Movie updated successfully."
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to movie_url(the_movie), alert: "Movie failed to update successfully." 
     end
   end
 
@@ -76,6 +76,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
